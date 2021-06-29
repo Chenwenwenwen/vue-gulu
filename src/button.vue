01a19c1cfe141e977eb273636cf1c33332746251
cwj <chenwenjing@yeezon.com>
2021-06-29 15:28:57 +0800

    按按钮button

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,66 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <!--slot 不能直接加class，外面套一层div-->
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-  export default {}
+export default {
+  props: ['icon', 'iconPosition']
+}
 </script>
 
 <style lang="scss">
-  .g-button{
-    font-size:var(--font-size);
-    height:var(--button-heighht);
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-heighht);
     padding: 0 1em;
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
-    &:hover{
+
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+
+    &:hover {
       border-color: var(--border-color-hover);
     }
-    &:active{
+
+    &:active {
       background-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .3em
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-left: .3em;
+        margin-right: 0;
+      }
+
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
