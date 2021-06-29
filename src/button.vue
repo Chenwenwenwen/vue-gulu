426a37a0df4829bf8eff62da2ebf305ed1ba4edd
cwj <chenwenjing@yeezon.com>
2021-06-29 17:18:59 +0800

    loading

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
-    <g-icon  name="loading" class="loading"></g-icon>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+    <g-icon  :name="icon" class="icon"  v-if="icon && !loading"></g-icon>
+    <g-icon  name="loading" class="loading icon" v-if="loading"></g-icon>
     <!--slot 不能直接加class，外面套一层div-->
     <div class="content">
       <slot></slot>
@@ -14,6 +14,10 @@ export default {
   // props: ['icon', 'iconPosition']
   props:{
     icon:{},
+    loading:{
+      type:Boolean,
+      default: false
+    },
     iconPosition:{
       type:String,
       default:'left',
@@ -44,28 +48,22 @@ export default {
     justify-content: center;
     align-items: center;
     vertical-align: middle;
-
     &:hover {
       border-color: var(--border-color-hover);
     }
-
     &:active {
       background-color: var(--button-active-bg);
     }
-
     &:focus {
       outline: none;
     }
-
     > .icon {
       order: 1;
       margin-right: .3em
     }
-
     > .content {
       order: 2;
     }
-
     &.icon-right {
       > .icon {
         order: 2;
